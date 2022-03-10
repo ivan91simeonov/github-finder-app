adeb074755f33802a7a3aa14f3e094d586ffde32
ivan91simeonov <vanches91@abv.bg>
2022-03-10 20:28:25 +0200

    loading spinner

---
+++ src/componets/layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner () {
+    return (
+        <div>
+            <img width={180} src={spinner} alt="Loading..." />
+        </div>
+    )
+}
+
+export default Spinner
--- src/componets/users/UserResults.jsx
+++ src/componets/users/UserResults.jsx
@@ -1,27 +1,34 @@
 import { useEffect, useState } from 'react';
+import Spinner from '../layout/Spinner';
 
 function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
-        fetchData()
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async() => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users` , {
-            headers:{
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        })
+  const fetchData = async () => {
+    const response = await fetch('https://api.github.com/users');
 
-        const data = await response.json()
+    const data = await response.json();
 
-        console.log(data)
-    }
-  return (
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
       <div>
-          
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
       </div>
-  )
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
